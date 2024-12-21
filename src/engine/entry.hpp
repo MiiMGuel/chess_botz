9812363635f233a41701ff760f1f01a2e548d978
MiiGuel <miiguel065+github.com@gmail.com>
2024-12-21 17:58:22 +0700

    WinMain

--- src/engine/entry.hpp
+++ src/engine/entry.hpp
@@ -16,13 +16,36 @@ public:
 #include <iostream>
 #include <stdexcept>
 
+#ifndef NOMINMAX
+#   define NOMINMAX
+#   define _NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
+#   define WIN32_LEAN_AND_MEAN
+#   define _WIN32_LEAN_AND_MEAN
+#endif
+#include <Windows.h>
+#ifdef _NOMINMAX
+#   undef NOMINMAX
+#   undef _NOMINMAX
+#endif
+#ifdef _WIN32_LEAN_AND_MEAN
+#   undef WIN32_LEAN_AND_MEAN
+#   undef _WIN32_LEAN_AND_MEAN
+#endif
+
 Program* MainProgram(int argc, char* argv[]);
 
-int main(int argc, char* argv[]) {
+int WINAPI WinMain(
+    HINSTANCE hInstance, 
+    HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, 
+    int nCmdShow
+) {
     Program* main = nullptr;
 
     try { 
-        main = MainProgram(argc, argv);
+        main = MainProgram(__argc, __argv);
         if (main == nullptr)
             throw std::runtime_error("Failed to initialize the main program.");
         
--- src/game.cpp
+++ src/game.cpp
@@ -3,17 +3,32 @@
 #define ENTRY_IMPL
 #include "engine/types.hpp"
 #include "engine/entry.hpp"
+#include "SDL2/SDL.h"
 
 class Game : public Program {
+private:
+    SDL_Window* window = nullptr;
+    SDL_Event   event  = { 0 };
+
 public:
     Game(int argc, char* argv[]) {
+        if (SDL_Init(SDL_INIT_VIDEO)) 
+            throw std::runtime_error(SDL_GetError());
+
+        window = SDL_CreateWindow(
+            "game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+            640, 480, SDL_WINDOW_RESIZABLE
+        ); if (window == nullptr) throw std::runtime_error(SDL_GetError());
     }
 
     ~Game() override {
+        if (window) SDL_DestroyWindow(window);
+        SDL_Quit();
     }
 
     void Run(void) override {
-        running = false;
+        while (SDL_PollEvent(&event))
+            if (event.type == SDL_QUIT) running = false;
     }
 };
 
